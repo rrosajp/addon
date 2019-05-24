6cb6414f5b7406b4c25e41b080fe6d6d32d3294c
greko <greco1712@email.it>
2019-05-24 08:53:44 +0200

    ripristino la versione esistente

--- channels/eurostreaming.py
+++ channels/eurostreaming.py
@@ -17,9 +17,8 @@
 
 import re
 
-from specials import autoplay
-from core import support
-from core import scrapertoolsV2, httptools, servertools, tmdb
+from specials import autoplay, filtertools
+from core import scrapertoolsV2, httptools, servertools, tmdb, support
 from core.item import Item
 from platformcode import logger, config
 
@@ -27,8 +26,8 @@ from platformcode import logger, config
 host = "https://eurostreaming.cafe/"
 headers = ['Referer', host]
 
-##IDIOMAS = {'Italiano': 'IT'}
-##list_language = IDIOMAS.values()
+IDIOMAS = {'Italiano': 'IT'}
+list_language = IDIOMAS.values()
 list_servers = ['verystream', 'wstream', 'speedvideo', 'flashx', 'nowvideo', 'streamango', 'deltabit', 'openload']
 list_quality = ['default']
 
@@ -76,7 +75,6 @@ def episodios(item):
     
     # Carica la pagina
     data = httptools.downloadpage(item.url).data
-    
     #======== 
     if 'clicca qui per aprire' in data.lower():
         item.url = scrapertoolsV2.find_single_match(data, '"go_to":"(.*?)"')
@@ -89,7 +87,6 @@ def episodios(item):
         data = httptools.downloadpage(item.url).data
     #=========
 
-##    data = scrapertoolsV2.decodeHtmlentities(data)
     matches = scrapertoolsV2.find_multiple_matches(data,
                                                    r'<span class="su-spoiler-icon"><\/span>(.*?)</div></div>')    
     for match in matches:
@@ -99,12 +96,12 @@ def episodios(item):
         logger.info("blocks log: %s" % ( blocks ))
         for block in blocks:
             season_n, episode_n = scrapertoolsV2.find_single_match(block, r'(\d+)(?:&#215;|×)(\d+)') 
-            titolo = scrapertoolsV2.find_single_match(block, r'[&#;]\d+[ ]([a-zA-Z0-9,’;&#.\s]+)[ ]?[^<]')
+            titolo = scrapertoolsV2.find_single_match(block, r'[&#;]\d+[ ]([a-zA-Z0-9;&#.\s]+)[ ]?[^<>]')
             logger.info("block log: %s" % ( block ))
                 
             titolo = re.sub(r'&#215;|×', "x", titolo).replace("&#8217;","'")
-##            item.infoLabels['season'] = season_n # permette di vedere il plot della stagione e...
-##            item.infoLabels['episode'] = episode_n # permette di vedere il plot della puntata e...
+            item.infoLabels['season'] = season_n # permette di vedere il plot della stagione e...
+            item.infoLabels['episode'] = episode_n # permette di vedere il plot della puntata e...
            
             itemlist.append(
                 Item(channel=item.channel,
@@ -112,21 +109,13 @@ def episodios(item):
                      contentType=item.contentType,
                      title="[B]" + season_n + "x" + episode_n + " " + titolo + "[/B] " + season_lang,
                      fulltitle=item.title, # Titolo nel video
-                     show=titolo + " : " + season_n + "x" + episode_n, # sottotitoletto nel video
+                     show=titolo + ":" + season_n + "x" + episode_n, # sottotitoletto nel video
                      url=block,
                      extra=item.extra,
                      thumbnail=item.thumbnail,
-                     infoLabels=item.infoLabels,
-                     language = season_lang,
-                     contentEpisodeTitle = titolo,
-                     contentSeason = season_n,  # permette di vedere il plot della stagione e...
-                     contentEpisodeNumber = episode_n # permette di vedere il plot della puntata e...
+                     infoLabels=item.infoLabels
                      ))
-            
-##            itemlist = support.scrape(item, patron_block='', patron=patron, listGroups=listGroups,
-##                                  patronNext=patronNext,
-##                                  action='episodios')                
-
+                
     tmdb.set_infoLabels_itemlist(itemlist, seekTmdb=True)
     
     support.videolibrary(itemlist, item)
