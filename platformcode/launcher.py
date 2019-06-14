61150c51038ce3631babe77f851a9c93698fd0ad
marco <m.toma99@gmail.com>
2019-06-14 21:13:17 +0200

    fix updater and re-add dev mode

--- platformcode/launcher.py
+++ platformcode/launcher.py
@@ -64,8 +64,10 @@ def run(item=None):
             else:
                 item = Item(channel="channelselector", action="getmainlist", viewmode="movie")
         if not config.get_setting('show_once'):
-            from platformcode import updater
-            updater.calcCurrHash()
+            if not os.path.isdir(config.get_runtime_path() + '/.git'):
+                logger.info("DEV MODE OFF")
+                from platformcode import updater
+                updater.calcCurrHash()
             from platformcode import xbmc_videolibrary
             xbmc_videolibrary.ask_set_content(1, config.get_setting('videolibrary_kodi_force'))
             config.set_setting('show_once', True)
