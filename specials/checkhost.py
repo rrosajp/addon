f391f7928d493e642a6e47b06e30239e75cf9dc1
greko17 <sex1712@email.it>
2019-08-01 17:26:38 +0200

    commenti vari in checkhost

--- specials/checkhost.py
+++ specials/checkhost.py
@@ -11,9 +11,9 @@
     i controlli di ip, asdl e dns.
     Questo perchè può succedere che in un qualsiasi momento
     la connessione può avere problemi.
-    Nel caso succeda il controllo e relativa scrittura del file viene interrotto
+    Nel caso accada un problema, il controllo e relativa scrittura del file viene interrotto
     con messaggio di avvertimento
-    
+
 """
 import xbmc
 import xbmcaddon
@@ -24,7 +24,7 @@ from kdicc import test_conn
 
 def check(item):
     logger.info()
-    
+
     folderJson = xbmc.translatePath(xbmcaddon.Addon().getAddonInfo('path')).decode('utf-8')
     fileJson = 'channels.json'    
 
@@ -33,18 +33,19 @@ def check(item):
 ##    logger.info("DATA :%s" % data)
 
     risultato = {}
-    
+
     for chann, host in sorted(data.items()):
         ris = []
-        logger.info("channel - host :%s - %s " % (chann, host))
-        
+        logger.info("check #### INIZIO #### channel - host :%s - %s " % (chann, host))
+
         #lst_host = []
         #lst_host.append(host)
         lst_host = [host]
+
         rslt = test_conn(is_exit = True, check_dns = False, view_msg = True,
                          lst_urls = lst_host, lst_site_check_dns = [], in_addon = True)
 
-        logger.info("checkhost rslt :%s  " % (rslt))
+        logger.info("check #### FINE #### rslt :%s  " % (rslt))
         rslt = rslt[0]
         # tutto ok
         if rslt['code'] == 200 and rslt['isRedirect'] == False:
@@ -57,7 +58,7 @@ def check(item):
             risultato[chann] = 'Host Sconosciuto - '+ str(rslt['code']) +' - '+ host
         else:
             # altri tipi di errore
-            risultato[chann] = str(rslt['code']) +' - '+ host
+            risultato[chann] = 'Errore Sconosciuto - '+str(rslt['code']) +' - '+ host
 
     fileJson_test = 'channels-test.json'
     # scrivo il file aggiornato
