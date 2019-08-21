468ee8bffeae23c8142213bb1023e5132301f9c8
mac12m99 <m.toma99@gmail.com>
2019-08-21 23:07:39 +0200

    Kdicc sotto thread, redirect solo per i dev e messaggio aggiornamenti a prova di nabbo

--- platformcode/launcher.py
+++ platformcode/launcher.py
@@ -34,9 +34,11 @@ def start():
     # se lo ha: non lo fa entrare nell'addon
     # se ha problemi di DNS avvia ma lascia entrare
     # se tutto ok: entra nell'addon
-    from specials.checkhost import test_conn, check_channels
-    check_adsl = test_conn(is_exit = True, check_dns = True, view_msg = True,
-              lst_urls = [], lst_site_check_dns = [], in_addon = True)
+    from specials.checkhost import test_conn
+    import threading
+    threading.Thread(target=test_conn, args=(True, True, True, [], [], True)).start()
+    # check_adsl = test_conn(is_exit = True, check_dns = True, view_msg = True,
+    #           lst_urls = [], lst_site_check_dns = [], in_addon = True)
 
     
 def run(item=None):
