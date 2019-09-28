fc020f6856f3d95c16f2dd1e9add1a2b402fd828
greko17 <sex1712@email.it>
2019-09-28 19:58:47 +0200

    fix: "al volo" per fare vedere i link vcrypt

--- channels/documentaristreamingda.py
+++ channels/documentaristreamingda.py
@@ -6,12 +6,15 @@
 import re
 import urlparse
 
-from core import httptools, scrapertools, servertools
+from core import httptools, scrapertools, servertools, support
 from core.item import Item
 from platformcode import logger, config
 
 __channel__ = "documentaristreamingda"
 host = config.get_channel_url(__channel__)
+
+list_servers = ['']
+list_quality = ['']
 
 def mainlist(item):
     logger.info("kod.documentaristreamingda mainlist")
@@ -156,105 +159,107 @@ def peliculas(item):
 
 def findvideos(item):
     logger.info("kod.documentaristreamingda findvideos")
+    return support.server(item)#, data= item.url)
 
-    data = httptools.downloadpage(item.url).data
-
-    links = []
-    begin = data.find('<div class="moview-details-text">')
-    if begin != -1:
-        end = data.find('<!-- //movie-details -->', begin)
-        mdiv = data[begin:end]
-
-        items = [[m.end(), m.group(1)] for m in re.finditer('<b style="color:#333333;">(.*?)<\/b>', mdiv)]
-        if items:
-            for idx, val in enumerate(items):
-                if idx == len(items) - 1:
-                    _data = mdiv[val[0]:-1]
-                else:
-                    _data = mdiv[val[0]:items[idx + 1][0]]
-
-                for link in re.findall('<a.*?href="([^"]+)"[^>]+>.*?<b>(.*?)<\/b><\/a>+', _data):
-                    if not link[0].strip() in [l[1] for l in links]: links.append(
-                        [val[1], link[0].strip(), link[1].strip()])
-
-        items = [[m.end(), m.group(1)] for m in re.finditer('<p><strong>(.*?)<\/strong><\/p>', mdiv)]
-        if items:
-            _title = ''
-            for idx, val in enumerate(items):
-                if idx == len(items) - 1:
-                    _data = mdiv[val[0]:-1]
-                else:
-                    _data = mdiv[val[0]:items[idx + 1][0]]
-
-                for link in re.findall('<a\s.*?href="([^"]+)".*?>(?:<span[^>]+>)*(?:<strong>)*([^<]+)', _data):
-                    if not link[0].strip() in [l[1] for l in links]:
-                        if not link[1].strip() in link[0]: _title = link[1].strip()
-                        links.append([_title, link[0].strip(), 'unknown'])
-
-        items = [[m.start(), m.group(1)] for m in re.finditer('<li><strong>([^<]+)<', mdiv)]
-        if items:
-            for idx, val in enumerate(items):
-                if idx == len(items) - 1:
-                    _data = mdiv[val[0]:-1]
-                else:
-                    _data = mdiv[val[0]:items[idx + 1][0]]
-
-                for link in re.findall('<a\s.*?href="([^"]+)".*?>(?:<span[^>]+>)*(?:<strong>)*([^<]+)', _data):
-                    if not link[0].strip() in [l[1] for l in links]: links.append(
-                        [val[1], link[0].strip(), link[1].strip()])
-
-    itemlist = []
-    if links:
-        for l in links:
-            title = unicode(l[0], 'utf8', 'ignore')
-            title = title.replace(u'\xa0', ' ').replace('Documentario ', '').replace(' doc ', ' ').replace(' streaming',
-                                                                                                           '').replace(
-                ' Streaming', '')
-            url = l[1]
-            action = "play"
-            server = "unknown"
-            folder = False
-
-            if url == '#' or not title: continue
-
-            logger.info('server: %s' % l[2])
-            if l[2] != 'unknown':
-                server = unicode(l[2], 'utf8', 'ignore')
-            else:
-                logger.info(url)
-                match = re.search('https?:\/\/(?:www\.)*([^\.]+)\.', url)
-                if match:
-                    server = match.group(1)
-
-                    if server == "documentari-streaming-db":
-                        action = "findvideos"
-                        folder = True
-            logger.info('server: %s, action: %s' % (server, action))
-
-            logger.info(title + ' - [COLOR blue]' + server + '[/COLOR]')
-
-            itemlist.append(Item(
-                channel=item.channel,
-                title=title + ' - [COLOR blue]' + server + '[/COLOR]',
-                action=action,
-                server=server,  # servertools.get_server_from_url(url),
-                url=url,
-                thumbnail=item.thumbnail,
-                fulltitle=title,
-                show=item.show,
-                plot=item.plot,
-                parentContent=item,
-                folder=folder)
-            )
-    else:
-        itemlist = servertools.find_video_items(data=data)
-
-        for videoitem in itemlist:
-            videoitem.title = "".join([item.title, '[COLOR green][B]' + videoitem.title + '[/B][/COLOR]'])
-            videoitem.fulltitle = item.fulltitle
-            videoitem.show = item.show
-            videoitem.thumbnail = item.thumbnail
-            videoitem.channel = item.channel
-
-    return itemlist
+##
+##    data = httptools.downloadpage(item.url).data
+##
+##    links = []
+##    begin = data.find('<div class="moview-details-text">')
+##    if begin != -1:
+##        end = data.find('<!-- //movie-details -->', begin)
+##        mdiv = data[begin:end]
+##
+##        items = [[m.end(), m.group(1)] for m in re.finditer('<b style="color:#333333;">(.*?)<\/b>', mdiv)]
+##        if items:
+##            for idx, val in enumerate(items):
+##                if idx == len(items) - 1:
+##                    _data = mdiv[val[0]:-1]
+##                else:
+##                    _data = mdiv[val[0]:items[idx + 1][0]]
+##
+##                for link in re.findall('<a.*?href="([^"]+)"[^>]+>.*?<b>(.*?)<\/b><\/a>+', _data):
+##                    if not link[0].strip() in [l[1] for l in links]: links.append(
+##                        [val[1], link[0].strip(), link[1].strip()])
+##
+##        items = [[m.end(), m.group(1)] for m in re.finditer('<p><strong>(.*?)<\/strong><\/p>', mdiv)]
+##        if items:
+##            _title = ''
+##            for idx, val in enumerate(items):
+##                if idx == len(items) - 1:
+##                    _data = mdiv[val[0]:-1]
+##                else:
+##                    _data = mdiv[val[0]:items[idx + 1][0]]
+##
+##                for link in re.findall('<a\s.*?href="([^"]+)".*?>(?:<span[^>]+>)*(?:<strong>)*([^<]+)', _data):
+##                    if not link[0].strip() in [l[1] for l in links]:
+##                        if not link[1].strip() in link[0]: _title = link[1].strip()
+##                        links.append([_title, link[0].strip(), 'unknown'])
+##
+##        items = [[m.start(), m.group(1)] for m in re.finditer('<li><strong>([^<]+)<', mdiv)]
+##        if items:
+##            for idx, val in enumerate(items):
+##                if idx == len(items) - 1:
+##                    _data = mdiv[val[0]:-1]
+##                else:
+##                    _data = mdiv[val[0]:items[idx + 1][0]]
+##
+##                for link in re.findall('<a\s.*?href="([^"]+)".*?>(?:<span[^>]+>)*(?:<strong>)*([^<]+)', _data):
+##                    if not link[0].strip() in [l[1] for l in links]: links.append(
+##                        [val[1], link[0].strip(), link[1].strip()])
+##
+##    itemlist = []
+##    if links:
+##        for l in links:
+##            title = unicode(l[0], 'utf8', 'ignore')
+##            title = title.replace(u'\xa0', ' ').replace('Documentario ', '').replace(' doc ', ' ').replace(' streaming',
+##                                                                                                           '').replace(
+##                ' Streaming', '')
+##            url = l[1]
+##            action = "play"
+##            server = "unknown"
+##            folder = False
+##
+##            if url == '#' or not title: continue
+##
+##            logger.info('server: %s' % l[2])
+##            if l[2] != 'unknown':
+##                server = unicode(l[2], 'utf8', 'ignore')
+##            else:
+##                logger.info(url)
+##                match = re.search('https?:\/\/(?:www\.)*([^\.]+)\.', url)
+##                if match:
+##                    server = match.group(1)
+##
+##                    if server == "documentari-streaming-db":
+##                        action = "findvideos"
+##                        folder = True
+##            logger.info('server: %s, action: %s' % (server, action))
+##
+##            logger.info(title + ' - [COLOR blue]' + server + '[/COLOR]')
+##
+##            itemlist.append(Item(
+##                channel=item.channel,
+##                title=title + ' - [COLOR blue]' + server + '[/COLOR]',
+##                action=action,
+##                server=server,  # servertools.get_server_from_url(url),
+##                url=url,
+##                thumbnail=item.thumbnail,
+##                fulltitle=title,
+##                show=item.show,
+##                plot=item.plot,
+##                parentContent=item,
+##                folder=folder)
+##            )
+##    else:
+##        itemlist = servertools.find_video_items(data=data)
+##
+##        for videoitem in itemlist:
+##            videoitem.title = "".join([item.title, '[COLOR green][B]' + videoitem.title + '[/B][/COLOR]'])
+##            videoitem.fulltitle = item.fulltitle
+##            videoitem.show = item.show
+##            videoitem.thumbnail = item.thumbnail
+##            videoitem.channel = item.channel
+##
+##    return itemlist
 
