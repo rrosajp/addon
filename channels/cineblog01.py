c457c5885724a27a31e42395ea4d65b30586df83
Alhaziel <alhaziel01@gmail.com>
2019-10-09 19:10:36 +0200

    Fix Novità CB01

--- channels/cineblog01.py
+++ channels/cineblog01.py
@@ -112,7 +112,7 @@ def newest(categoria):
     item = Item()
     item.contentType = 'movie'
     item.url = host + '/lista-film-ultimi-100-film-aggiunti/'
-    return support.scrape(item, r'<a href=([^>]+)>([^<([]+)(?:\[([A-Z]+)\])?\s\(([0-9]{4})\)<\/a>',
+    return support.scrape(item, r'<a href="([^"]+)">([^<([]+)(?:\[([A-Z]+)\])?\s\(([0-9]{4})\)<\/a>',
                    ['url', 'title', 'quality', 'year'],
                    patron_block=r'Ultimi 100 film aggiunti:.*?<\/td>')
 
