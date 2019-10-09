a06a98d6b48bf990d1b23e085426e192cb50c52f
Alhaziel <alhaziel01@gmail.com>
2019-10-09 18:25:46 +0200

    Fix CB01

--- channels/cineblog01.py
+++ channels/cineblog01.py
@@ -18,7 +18,10 @@ headers = ""
 def findhost():
     global host, headers
     permUrl = httptools.downloadpage('https://www.cb01.uno/', follow_redirects=False).headers
-    host = 'https://www.'+permUrl['location'].replace('https://www.google.it/search?q=site:', '')
+    if host[:4] != 'http':
+        host = 'https://'+permUrl['location'].replace('https://www.google.it/search?q=site:', '')
+    else:
+        host = permUrl['location'].replace('https://www.google.it/search?q=site:', '')
     headers = [['Referer', host]]
 
 list_servers = ['verystream', 'openload', 'streamango', 'wstream']
