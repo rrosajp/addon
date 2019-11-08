56f3ade3f03e7f02a48eda2e87455ca7fa8115e5
Alhaziel <alhaziel01@gmail.com>
2019-11-08 10:28:12 +0100

    Fix Supervideo

--- servers/supervideo.py
+++ servers/supervideo.py
@@ -21,14 +21,20 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.info("url=" + page_url)
     video_urls = []
     data = httptools.downloadpage(page_url).data
-    code = jsunpack.unpack(scrapertoolsV2.find_single_match(data, "<script type='text/javascript'>(eval.*)"))
-    match = scrapertoolsV2.find_single_match(code, 'sources:(\[[^]]+\])')
-    lSrc = ast.literal_eval(match)
+    code_data = scrapertoolsV2.find_single_match(data, "<script type='text/javascript'>(eval.*)")
+    if code_data:
+        code = jsunpack.unpack(code_data)
+        match = scrapertoolsV2.find_single_match(code, r'sources:(\[[^]]+\])')
+        lSrc = ast.literal_eval(match)
 
-    lQuality = ['360p', '720p', '1080p', '4k'][:len(lSrc)-1]
-    lQuality.reverse()
+        lQuality = ['360p', '720p', '1080p', '4k'][:len(lSrc)-1]
+        lQuality.reverse()
 
-    for n, source in enumerate(lSrc):
-        quality = 'auto' if n==0 else lQuality[n-1]
-        video_urls.append(['.' + source.split('.')[-1] + '(' + quality + ') [SuperVideo]', source])
+        for n, source in enumerate(lSrc):
+            quality = 'auto' if n==0 else lQuality[n-1]
+            video_urls.append(['.' + source.split('.')[-1] + '(' + quality + ') [SuperVideo]', source])
+    else:
+        matches = scrapertoolsV2.find_multiple_matches(data, r'src:\s*"([^"]+)",\s*type:\s*"[^"]+"\s*, res:\s(\d+)')
+        for url, quality in matches:
+            video_urls.append(['mp4 [' + quality + ']', url])
     return video_urls
