dc17c8350c6346cdd67de28f167a3715723a1b5b
Alhaziel <alhaziel01@gmail.com>
2019-11-12 19:25:46 +0100

    Fix Wstream (Visualizza risoluzione)

--- servers/wstream.py
+++ servers/wstream.py
@@ -2,11 +2,10 @@
 # Kodi on Demand - Kodi Addon - Kodi Addon
 # by DrZ3r0 - Fix Alhaziel
 
-import re
-import urllib
+import re, json, urllib
 
 from core import httptools, scrapertools
-from platformcode import logger
+from platformcode import logger, config
 
 headers = [['User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0']]
 
@@ -14,13 +13,13 @@ def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
     data = httptools.downloadpage(page_url).data
     if "Not Found" in data or "File was deleted" in data:
-        return False, "[wstream.py] Il File Non esiste"
+        return False, config.get_localized_string(70449) % 'Wstream'
     return True, ""
 
 
 # Returns an array of possible video url's from the page_url
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
-    logger.info("[wstream.py] url=" + page_url)
+    logger.info("[Wstream] url=" + page_url)
     video_urls = []
 
     if '/streaming.php' in page_url:
@@ -28,33 +27,27 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
         page_url = 'https://wstream.video/video.php?file_code=' + code
 
     code = page_url.split('=')[-1]
-    post = urllib.urlencode({
-        'videox': code
-    })
+    post = urllib.urlencode({'videox': code})
 
-    data = httptools.downloadpage(page_url, headers=headers, post=post, follow_redirects=True).data.replace('https','http')
-    logger.info("[wstream.py] data=" + data)
-    vid = scrapertools.find_multiple_matches(data, 'download_video.*?>.*?<.*?<td>([^\,,\s]+)')
+    data = httptools.downloadpage(page_url, headers=headers, post=post, follow_redirects=True).data
     headers.append(['Referer', page_url])
-    post_data = scrapertools.find_single_match(data,"</div>\s*<script type='text/javascript'>(eval.function.p,a,c,k,e,.*?)\s*</script>")
+    _headers = urllib.urlencode(dict(headers))
+
+    post_data = scrapertools.find_single_match(data, r"</div>\s*<script type='text/javascript'>(eval.function.p,a,c,k,e,.*?)\s*</script>")
     if post_data != "":
         from lib import jsunpack
         data = jsunpack.unpack(post_data)
-    logger.info("[wstream.py] data=" + data)
-    block = scrapertools.find_single_match(data, 'sources:\s*\[[^\]]+\]')
-    data = block
 
-    media_urls = scrapertools.find_multiple_matches(data, '(http.*?\.mp4)')
-    _headers = urllib.urlencode(dict(headers))
-    i = 0
+        data = scrapertools.find_single_match(data, r'sources:\s*(\[[^\]]+\])')
+        data = re.sub('([A-z]+):(?!/)','"\\1":',data)
+        keys = json.loads(data)
 
-    for media_url in media_urls:
-        video_urls.append([vid[i] if vid else 'video' + " mp4 [wstream] ", media_url + '|' + _headers])
-        i = i + 1
+        for key in keys:
+            video_urls.append(['%s [%sp]' % (key['type'].replace('video/',''), key['label']), key['src'].replace('https','http') + '|' + _headers])
+    else:
+        media_urls = scrapertools.find_multiple_matches(data, '(http.*?\.mp4)')
 
-    for video_url in video_urls:
-        logger.info("[wstream.py] %s - %s" % (video_url[0], video_url[1]))
-
-    logger.info(video_urls)
+        for media_url in media_urls:
+            video_urls.append(['video' + " mp4 [wstream] ", media_url + '|' + _headers])
 
     return video_urls
