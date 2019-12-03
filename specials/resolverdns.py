191e30201c22061d15d77f32e655daab7ffc16fc
greko17 <sex1712@email.it>
2019-12-03 12:25:53 +0100

    fix: ResolverDNS
    Possibilità di scegliere il provider nelle impostazioni alla voce DNS
    spostato abilita check DNS nella relativa voce in impostazioni

--- specials/resolverdns.py
+++ specials/resolverdns.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
-# -*- CLOUDFLARE'S DNS -*-
+# -*- OVERRIDE RESOLVE DNS -*-
 
-##from lib import dns
-##import dns.resolver
-##from dns.resolver import override_system_resolver
-##import socket
-##import requests
-##
-##def dns_resolver():
-##    res = dns.resolver.Resolver(configure=True)
-##    res.nameservers = ['1.1.1.1', '2606:4700:4700::1111',
-##                       '1.0.0.1', '2606:4700:4700::1001']
-##    override_system_resolver(res)
+from platformcode import config
+from core import support
 
-## Da prevedere di aggiungere la possibilità
-## di inserire i dns a piacere degli utenti!!!
+active_dns = config.get_setting('resolver_dns')
 
-from lib import dns
-import dns.resolver
-from dns.resolver import override_system_resolver
-import dns.name
-import socket
-import requests
+if active_dns == True:
 
-res = dns.resolver.Resolver(configure=True)
+    from lib import dns
+    import dns.resolver
+    from dns.resolver import override_system_resolver
+    import dns.name
+    import socket
+    import requests
 
-res.nameservers = ['1.1.1.1', '2606:4700:4700::1111',
-                   '1.0.0.1', '2606:4700:4700::1001' ]
+    res = dns.resolver.Resolver(configure=True)
 
-override_system_resolver(res)
+    """
+        legge le impostazioni dalla configurazione
+        e setta i relativi DNS
+    """
+    nameservers_dns = config.get_setting('resolver_dns_service')
+
+    if nameservers_dns == 'Google':
+        res.nameservers_dns = ['8.8.8.8', '2001:4860:4860::8888',
+                       '8.8.4.4', '2001:4860:4860::8844' ]
+    elif nameservers_dns == 'OpenDns':
+        res.nameservers_dns = ['208.67.222.222', '2620:119:35::35',
+                       '208.67.222.220', '2620:119:53::53' ]
+    else:# resolver_dns_service == 'Cloudflare':
+        res.nameservers = ['1.1.1.1', '2606:4700:4700::1111',
+                       '1.0.0.1', '2606:4700:4700::1001' ]
+
+    override_system_resolver(res)
+
+
