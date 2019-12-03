3b533d4ef9ffa56f4f7989a08354bdf77f5b8292
greko17 <sex1712@email.it>
2019-12-03 17:18:52 +0100

    fix: resolver dns
    aggiunti opendns per categoria

--- specials/resolverdns.py
+++ specials/resolverdns.py
@@ -25,13 +25,19 @@ if active_dns == True:
 
     if nameservers_dns == 'Google':
         res.nameservers_dns = ['8.8.8.8', '2001:4860:4860::8888',
-                       '8.8.4.4', '2001:4860:4860::8844' ]
-    elif nameservers_dns == 'OpenDns':
-        res.nameservers_dns = ['208.67.222.222', '2620:119:35::35',
-                       '208.67.222.220', '2620:119:53::53' ]
+                       '8.8.4.4', '2001:4860:4860::8844']
+    elif nameservers_dns == 'OpenDns Home ip(v4)':
+        res.nameservers_dns = ['208.67.222.222', '208.67.222.220']
+    elif nameservers_dns == 'OpenDns Home VIP ip(v4)':
+        res.nameservers_dns = ['208.67.222.222', '208.67.222.220']
+    elif nameservers_dns == 'OpenDns Family Shield ip(v4)':
+        res.nameservers_dns = ['208.67.222.123', '208.67.220.123']
+    elif nameservers_dns == 'OpenDns ip(v6)':
+        #https://support.opendns.com/hc/en-us/articles/227986667-Does-OpenDNS-Support-IPv6-
+        res.nameservers_dns = ['2620:119:35::35', '2620:119:53::53']
     else:# resolver_dns_service == 'Cloudflare':
         res.nameservers = ['1.1.1.1', '2606:4700:4700::1111',
-                       '1.0.0.1', '2606:4700:4700::1001' ]
+                       '1.0.0.1', '2606:4700:4700::1001']
 
     override_system_resolver(res)
 
