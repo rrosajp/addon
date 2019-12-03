917f1a2c4b5bd25ddf5136bd53b3788918c051fe
Alhaziel <alhaziel01@gmail.com>
2019-12-03 18:47:48 +0100

    Revert "Test - CF per XBOX"
    This reverts commit ef822e749d7275c118145c8cc599ab33fa6d7144.

--- core/httptools.py
+++ core/httptools.py
@@ -409,8 +409,7 @@ def downloadpage(url, **opt):
         """
     load_cookies()
     import requests
-    if ('Linux' in os.name and 'ANDROID_STORAGE' not in os.environ and config.get_platform(True)['num_version'] > 18.2) \
-        or ('nt' in os.name and 'ProfileDrive' in os.environ and 'LogDrive' in os.environ):
+    if 'Linux' in os.name and 'ANDROID_STORAGE' not in os.environ and config.get_platform(True)['num_version'] > 18.2:
         from lib.cloudscraper import cloudscraper
     else:
         from lib.cloudscraper import cloudscraper_mod as cloudscraper
--- lib/cloudscraper/cloudscraper/interpreters/js2py.py
+++ lib/cloudscraper/cloudscraper/interpreters/js2py.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from js2py import js2py
+import js2py
 import logging
 import base64
 
