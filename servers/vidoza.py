d6c94f493f5052420d04159a8631d888423456af
Alhaziel <alhaziel01@gmail.com>
2020-01-17 16:27:47 +0100

    Fix e Migliorie a Cloudvideo, Supervideo e vidoza

--- servers/vidoza.py
+++ servers/vidoza.py
@@ -8,6 +8,7 @@ from platformcode import logger
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
+    global data
     data = httptools.downloadpage(page_url).data
     if "Page not found" in data or "File was deleted" in data:
         return False, "[vidoza] El archivo no existe o ha sido borrado"
@@ -19,7 +20,7 @@ def test_video_exists(page_url):
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
     logger.info("(page_url='%s')" % page_url)
-    data = httptools.downloadpage(page_url).data
+    global data
     video_urls = []
 
     s = scrapertools.find_single_match(data, r'sourcesCode\s*:\s*(\[\{.*?\}\])')
@@ -30,8 +31,8 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
             if 'src' in enlace or 'file' in enlace:
                 url = enlace['src'] if 'src' in enlace else enlace['file']
                 tit = ''
-                if 'label' in enlace: tit += '[%s]' % enlace['label']
-                if 'res' in enlace: tit += '[%s]' % enlace['res']
+                if 'label' in enlace: tit += ' [%s]' % enlace['label']
+                if 'res' in enlace: tit += ' [%s]' % enlace['res']
                 if tit == '' and 'type' in enlace: tit = enlace['type']
                 if tit == '': tit = '.mp4'
 
