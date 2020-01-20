c785e5abdd0c33f556c2fef3c3e24dca875c13fb
marco <m.toma99@gmail.com>
2020-01-20 22:54:16 +0100

    pezza

--- core/support.py
+++ core/support.py
@@ -168,7 +168,10 @@ def scrapeLang(scraped, lang, longtitle):
     return language, longtitle
 
 def cleantitle(title):
-    cleantitle = scrapertools.htmlclean(scrapertools.decodeHtmlentities(title).replace('"', "'").replace('×', 'x').replace('–', '-')).strip()
+    try:
+        cleantitle = scrapertools.htmlclean(scrapertools.decodeHtmlentities(title).replace('"', "'").replace('×', 'x').replace('–', '-')).strip()
+    except:
+        cleantitle = title
     return cleantitle
 
 def scrapeBlock(item, args, block, patron, headers, action, pagination, debug, typeContentDict, typeActionDict, blacklist, search, pag, function, lang):
