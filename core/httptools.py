53691c917db951038ac4c96175f05f8d1517e03b
marco <m.toma99@gmail.com>
2020-02-04 18:11:49 +0100

    fix piratestreaming (CF)

--- core/httptools.py
+++ core/httptools.py
@@ -254,7 +254,7 @@ def downloadpage(url, **opt):
     domain = urlparse.urlparse(url).netloc
     CF = False
     if domain in ['www.guardaserie.media', 'casacinema.space', 'wstream.video', 'akvideo.stream', 'backin.net',
-                  'dreamsub.stream', 'altadefinizione-nuovo.link', 'ilgeniodellostreaming.si']:
+                  'dreamsub.stream', 'altadefinizione-nuovo.link', 'ilgeniodellostreaming.si', 'www.piratestreaming.gratis']:
         from lib import cloudscraper
         session = cloudscraper.create_scraper()
         CF = True
