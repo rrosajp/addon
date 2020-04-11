d191dcf21d01e17f429d2443c3170a115e0d1d45
Alhaziel01 <alhaziel01@gmail.com>
2020-04-11 15:32:46 +0200

    Fix Fembed

--- servers/fembed.py
+++ servers/fembed.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import re
 from core import httptools
 from core import jsontools
 from platformcode import logger, config
 
 def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
+    global data
+
+    page_url = re.sub('://[^/]+/', '://feurl.com/', page_url)
     data = httptools.downloadpage(page_url).data
     if "Sorry 404 not found" in data or "This video is unavailable" in data or "Sorry this video is unavailable:" in data:
         return False, "[fembed] El fichero ha sido borrado"
     page_url = page_url.replace("/f/","/v/")
     page_url = page_url.replace("/v/","/api/source/")
-    data = httptools.downloadpage(page_url, post={}).data
+    data = httptools.downloadpage(page_url, post={}).json
+    logger.info(data)
     if "Video not found or" in data or "We are encoding this video" in data:
         return False, config.get_localized_string(70449) % "fembed"
     return True, ""
@@ -20,10 +25,6 @@ def test_video_exists(page_url):
 def get_video_url(page_url, user="", password="", video_password=""):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
-    page_url = page_url.replace("/f/","/v/")
-    page_url = page_url.replace("/v/","/api/source/")
-    data = httptools.downloadpage(page_url, post={}).data
-    data = jsontools.load(data)
     for videos in data["data"]:
         v = videos["file"]
         if not v.startswith("http"): v = "https://www.fembed.com" + videos["file"]
