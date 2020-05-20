747c0e5531a853f091469021c59f2fff7e2ee8c1
Alhaziel01 <alhaziel01@gmail.com>
2020-05-20 19:02:58 +0200

    Fix Netfreex

--- channels/netfreex.py
+++ channels/netfreex.py
@@ -7,7 +7,10 @@ from core import support
 from core.item import Item
 from platformcode import logger, config
 
-host = config.get_channel_url()
+def findhost():
+    return 'https://' + support.match('https://netfreex.uno/', patron='value="site:([^"]+)"').match
+
+host = config.get_channel_url(findhost)
 headers = ""
 
 IDIOMAS = {'Italiano': 'IT'}
