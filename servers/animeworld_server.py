bb316e66b342068ec6b00519edc40c962ccf434c
Alhaziel01 <alhaziel01@gmail.com>
2020-05-27 18:23:00 +0200

    Ottimizzati alcuni server

--- servers/animeworld_server.py
+++ servers/animeworld_server.py
@@ -13,6 +13,7 @@ def test_video_exists(page_url):
     logger.info("(page_url='%s')" % page_url)
     post = urllib.urlencode({'r': '', 'd': 'animeworld.biz'})
     data_json = httptools.downloadpage(page_url.replace('/v/', '/api/source/'), headers=[['x-requested-with', 'XMLHttpRequest']], post=post).data
+    global json
     json = jsontools.load(data_json)
     if not json['data']:
         return False, config.get_localized_string(70449) % "AnimeWorld"
@@ -23,9 +24,7 @@ def test_video_exists(page_url):
 def get_video_url(page_url, user="", password="", video_password=""):
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
-    post = urllib.urlencode({'r': '', 'd': 'animeworld.biz'})
-    data_json = httptools.downloadpage(page_url.replace('/v/', '/api/source/'), headers=[['x-requested-with', 'XMLHttpRequest']], post=post).data
-    json = jsontools.load(data_json)
+    global json
     if json['data']:
         for file in json['data']:
             media_url = file['file']
