598b395d9642448bac2da78d23daea9ef2de4115
Dexter Morgan <9434916+32dexter@users.noreply.github.com>
2020-06-13 10:39:47 +0200

    aggiunti canali guardaserieCam e guardaserieIcu

---
+++ channels/guardaserieCam.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# Canale per 'guardaserie_live'
+# By: Napster32
+# ------------------------------------------------------------
+# Rev: 0.0
+# Update 11-06-2020
+# fix:
+# 1. Emissione
+
+# possibilità di miglioramento: inserire menu per genere - lista serie tv e gestire le novità
+
+from core import support
+from core.support import log
+from platformcode import logger, config
+
+host = config.get_channel_url()
+headers = [['Referer', host]]
+
+list_servers = ['supervideo']
+list_quality = ['default']
+
+
+@support.menu
+def mainlist(item):
+    tvshow = ['/serietv-streaming',
+              ('Per Lettera', ['/serietv-streaming/A', 'list', 'Serie-Tv per Lettera'])
+              ]
+    return locals()
+
+
+@support.scrape
+def list(item):
+    patronMenu = r'<a title="(?P<title>[^"]+)" href="(?P<url>[^"]+)'
+    action = 'peliculas'
+    return locals()
+
+
+@support.scrape
+def peliculas(item):
+    patron = r'<div class="mlnh-thumb"><a href="(?P<url>[^"]+).*?title="(?P<title>[^"]+).*?src="(?P<thumb>[^"]+).*?hdn">(?P<year>[0-9]{4})'
+    patronNext = 'pagenavi.*?<span>.</span>.*?<a href="([^"]+)'
+    action = 'episodios'
+    return locals()
+
+
+@support.scrape
+def episodios(item):
+    patronBlock = r'<div class="tab-pane fade" id="season-(?P<season>.)"(?P<block>.*?)</div>'
+    patron = r'<a href="#" allowfullscreen data-link="(?P<url>[^"]+).*?title="(?P<title>[^"]+)(?P<lang>[sS][uU][bB]-?[iI][tT][aA])?\s*">(?P<episode>[^<]+)'
+    action = 'findvideos'
+    # debug = True
+    return locals()
+
+
+def search(item, text):
+    support.log('search', item)
+    item.contentType = 'tvshow'
+    itemlist = []
+    text = text.replace(' ', '+')
+    item.url = host + '/index.php?story=%s&do=search&subaction=search' % (text)
+    try:
+        return peliculas(item)
+    except:
+        import sys
+        for line in sys.exc_info():
+            log('search log:', line)
+        return []
+
+
+def findvideos(item):
+    logger.info("[guardaserie_live] findvideos")
+    return support.server(item, item.url)
---
+++ channels/guardaserieIcu.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# Canale per 'Guarda_Serie'
+# By: Napster32
+# ------------------------------------------------------------
+# Rev: 0.0
+# Update 11-06-2020
+# fix:
+# 1. Emissione
+
+# possibilità di miglioramento: gestire le novità (sezione Ultimi episodi sul sito)
+
+from core.support import log
+from core import support
+from platformcode import config
+
+host = config.get_channel_url()
+headers = [['Referer', host]]
+
+list_servers = ['mixdrop,vudeo,doodstream,onlystream']
+list_quality = ['default']
+
+@support.menu
+def mainlist(item):
+    tvshow = ['/serie']
+    return locals()
+
+@support.scrape
+def peliculas(item):
+    patronBlock = r'movies-list movies-list-full(?P<block>.*?)footer>'
+    patron = r'<div data-movie-id.*?a href="(?P<url>[^"]+).*?<img data-original="(?P<thumbnail>[^"]+).*?qtip-title">(?P<title>[^<]+).*?(?:rel="tag">(?P<year>[0-9]{4}))?</a>'
+    patronNext = '<li class=.active.>.*?href=.(.*?).>'
+    action = 'episodios'
+    return locals()
+
+@support.scrape
+def episodios(item):
+    patronBlock = r'<strong>Stagione (?P<season>[0-9]+)(?P<block>.*?)</div></div>'
+    patron = r'<a href="(?P<url>[^"]+)">\s*Episodio\s*(?P<episode>[0-9]+)'
+    return locals()
+
+def search(item, text):
+    log(text)
+    itemlist = []
+    text = text.replace(' ', '+')
+    item.url = host + "/?s=" + text
+    try:
+        item.args = 'search'
+        return peliculas(item)
+    except:
+        import sys
+        for line in sys.exc_info():
+            log("%s" % line)
+
+    return []
+
+def findvideos(item):
+    support.log('findvideos', item)
+    return support.server(item, headers=headers)
