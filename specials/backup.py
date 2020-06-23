9f262dc605b8597f8b6e878e2a3c96dae1c9dfe4
Alhaziel01 <alhaziel01@gmail.com>
2020-06-23 12:30:53 +0200

    Fix Backup Videoteca con caratteri speciali

--- specials/backup.py
+++ specials/backup.py
@@ -12,9 +12,11 @@ from platformcode import logger, config, platformtools, xbmc_videolibrary
 from distutils.dir_util import copy_tree
 from specials import videolibrary
 
-temp_path = xbmc.translatePath("special://userdata/addon_data/plugin.video.kod/temp/")
-movies_path = os.path.join(temp_path, "movies")
-tvshows_path = os.path.join(temp_path, "tvshows")
+temp_path = u'' + xbmc.translatePath("special://userdata/addon_data/plugin.video.kod/temp/")
+movies_path = u'' + filetools.join(temp_path, "movies")
+tvshows_path = u'' + filetools.join(temp_path, "tvshows")
+videolibrary_movies_path = u'' + videolibrarytools.MOVIES_PATH
+videolibrary_tvshows_path = u'' + videolibrarytools.TVSHOWS_PATH
 
 
 def export_videolibrary(item):
@@ -23,7 +25,7 @@ def export_videolibrary(item):
     zip_file_folder = platformtools.dialog_browse(3, config.get_localized_string(80002))
     if zip_file_folder == "":
         return
-    zip_file = xbmc.translatePath(zip_file_folder + "KoD_video_library-" + str(datetime.date.today()) + ".zip")
+    zip_file = u'' + xbmc.translatePath(zip_file_folder + "KoD_video_library-" + str(datetime.date.today()) + ".zip")
 
     p_dialog = platformtools.dialog_progress_bg(config.get_localized_string(20000), config.get_localized_string(80003))
     p_dialog.update(0)
@@ -33,10 +35,10 @@ def export_videolibrary(item):
     filetools.mkdir(temp_path)
     p_dialog.update(25)
     filetools.mkdir(movies_path)
-    copy_tree(videolibrarytools.MOVIES_PATH, movies_path)
+    copy_tree(videolibrary_movies_path, movies_path)
     p_dialog.update(50)
     filetools.mkdir(tvshows_path)
-    copy_tree(videolibrarytools.TVSHOWS_PATH, tvshows_path)
+    copy_tree(videolibrary_tvshows_path, tvshows_path)
     p_dialog.update(75)
 
     zipper = ziptools.ziptools()
@@ -53,7 +55,7 @@ def export_videolibrary(item):
 def import_videolibrary(item):
     logger.info()
 
-    zip_file = platformtools.dialog_browse(1, config.get_localized_string(80005))
+    zip_file = u'' + platformtools.dialog_browse(1, config.get_localized_string(80005))
     if zip_file == "":
         return
     if not platformtools.dialog_yesno(config.get_localized_string(20000), config.get_localized_string(80006)):
@@ -73,16 +75,16 @@ def import_videolibrary(item):
     if config.is_xbmc() and config.get_setting("videolibrary_kodi"):
         xbmc_videolibrary.clean()
     p_dialog.update(30)
-    filetools.rmdirtree(videolibrarytools.MOVIES_PATH)
-    filetools.rmdirtree(videolibrarytools.TVSHOWS_PATH)
+    filetools.rmdirtree(videolibrary_movies_path)
+    filetools.rmdirtree(videolibrary_tvshows_path)
     p_dialog.update(50)
 
     config.verify_directories_created()
     if filetools.exists(movies_path):
-        copy_tree(movies_path, videolibrarytools.MOVIES_PATH)
+        copy_tree(movies_path, videolibrary_movies_path)
     p_dialog.update(70)
     if filetools.exists(tvshows_path):
-        copy_tree(tvshows_path, videolibrarytools.TVSHOWS_PATH)
+        copy_tree(tvshows_path, videolibrary_tvshows_path)
     p_dialog.update(90)
     filetools.rmdirtree(temp_path)
 
