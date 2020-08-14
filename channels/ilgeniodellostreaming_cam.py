919e5341c395115a7efb0cd30844c95f2256afd9
Alhaziel01 <alhaziel01@gmail.com>
2020-08-14 19:27:58 +0200

    Aggiunto iIlGenioDelloStreaming CAM

---
+++ channels/ilgeniodellostreaming_cam.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------
+# Canale per ilgeniodellostreaming_cam
+# ------------------------------------------------------------
+
+
+from core import support
+from core.support import log
+from core.item import Item
+from platformcode import config
+
+host = config.get_channel_url()
+headers = [['Referer', host]]
+
+@support.menu
+def mainlist(item):
+    film = ['/film/',
+           ('In Sala', ['', 'peliculas', 'sala']),
+           ('Generi',['', 'genres', 'genres']),
+           ('Per Lettera',['/catalog/all', 'genres', 'az']),
+           ('Anni',['', 'genres', 'year'])]
+
+    return locals()
+
+
+@support.scrape
+def peliculas(item):
+    if item.args == 'sala':
+        patronBlock = r'insala(?P<block>.*?)<header>'
+        patron = r'<img src="(?P<thumb>[^"]+)[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>\s*(?P<rating>[^<]+)[^>]+>[^>]+>(?P<quality>[^<]+)[^>]+>[^>]+>[^>]+>[^>]+><a href="(?P<url>[^"]+)">(?P<title>[^<]+)[^>]+>[^>]+>[^>]+>(?P<year>\d{4})'
+    elif item.args == 'az':
+        patron = r'<img src="(?P<thumb>[^"]+)[^>]+>[^>]+>[^>]+>[^>]+><a href="(?P<url>[^"]+)[^>]+>(?P<title>[^<]+)<[^>]+>[^>]+>[^>]+>.*?<span class="labelimdb">(?P<rating>[^>]+)<'
+    else:
+        patron = r'<img src="(?P<thumb>[^"]+)[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>\s*(?P<rating>[^<]+)[^>]+>[^>]+>(?P<quality>[^<]+)[^>]+>[^>]+>[^>]+>[^>]+><a href="(?P<url>[^"]+)">(?P<title>[^<]+)[^>]+>[^>]+>[^>]+>(?P<year>\d{4})[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>[^>]+>\s*(?P<plot>[^<]+)<[^>]+>'
+
+    patronNext = 'href="([^>]+)">»'
+
+    return locals()
+
+
+@support.scrape
+def genres(item):
+    action='peliculas'
+    if item.args == 'genres':
+        patronBlock = r'<div class="sidemenu">\s*<h2>Genere</h2>(?P<block>.*?)</ul'
+    elif item.args == 'year':
+        item.args = 'genres'
+        patronBlock = r'<div class="sidemenu">\s*<h2>Anno di uscita</h2>(?P<block>.*?)</ul'
+    elif item.args == 'az':
+        patronBlock = r'<div class="movies-letter">(?P<block>.*?)<div class="clearfix">'
+
+    patronMenu = r'<a(?:.+?)?href="(?P<url>.*?)"[ ]?>(?P<title>.*?)<\/a>'
+
+    return locals()
+
+def search(item, text):
+    log(text)
+    text = text.replace(' ', '+')
+    item.url = host + "/search/" + text
+    try:
+        return peliculas(item)
+    except:
+        import sys
+        for line in sys.exc_info():
+            log("%s" % line)
+
+    return []
+
+def newest(categoria):
+    log(categoria)
+    itemlist = []
+    item = Item()
+
+    if categoria == 'peliculas':
+        item.contentType = 'movie'
+        item.url = host + '/film/'
+    try:
+        item.action = 'peliculas'
+        itemlist = peliculas(item)
+
+    except:
+        import sys
+        for line in sys.exc_info():
+            log("{0}".format(line))
+        return []
+
+    return itemlist
+
+
+def findvideos(item):
+    log()
+    return support.server(item)
