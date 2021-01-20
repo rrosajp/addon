96d3692522c6ef359ef28de993cfa5d01e836ccd
Alhaziel01 <alhaziel01@gmail.com>
2021-01-20 19:54:17 +0100

    - Rimosso Server Streaming Community  - Aggiunta Possibilità di Passare video_urls come parametro

--- channels/streamingcommunity.py
+++ channels/streamingcommunity.py
@@ -4,7 +4,7 @@
 # ------------------------------------------------------------
 
 import json, requests, sys
-from core import support
+from core import support, channeltools
 from platformcode import logger
 if sys.version_info[0] >= 3:
     from concurrent import futures
@@ -190,5 +190,21 @@ def episodios(item):
 
 
 def findvideos(item):
-    itemlist = [item.clone(title = 'StreamingCommunity', server='streamingcommunity_server')]
-    return support.server(item, item.url)
+    video_urls = []
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14',
+           'content-type': 'application/json;charset=UTF-8',
+           'Referer': 'https://streamingcommunity.net'}
+    data = support.match(item, headers=headers).data.replace('&quot;','"').replace('\\','')
+    url = support.match(data, patron=r'video_url"\s*:\s*"([^"]+)"').match
+    def videourls(res):
+        newurl = '{}/{}'.format(url, res)
+        if requests.head(newurl, headers=headers).status_code == 200:
+            video_urls.append(["m3u8 {} [StreamingCommunity]".format(res), newurl])
+
+    with futures.ThreadPoolExecutor() as executor:
+        for res in ['480p', '720p', '1080p']:
+            executor.submit(videourls, res) 
+    if not video_urls: video_urls = [["m3u8 [StreamingCommunity]", url]]
+    else: video_urls.sort(key=lambda url: int(support.match(url[0], patron=r'(\d+)p').match))
+    itemlist = [item.clone(title = 'StreamingCommunity', video_urls=video_urls, thumbnail=channeltools.get_channel_parameters(item.channel)["thumbnail"], forcethumb=True)]
+    return support.server(item, itemlist=itemlist)
