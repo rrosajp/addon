bad099605cfcdfd4f4da53d502803d6569f12531
Alhaziel01 <alhaziel01@gmail.com>
2021-02-20 10:59:26 +0100

    - Fix ricerca SerieHD  - Fix TMDB

--- channels/seriehd.py
+++ channels/seriehd.py
@@ -31,7 +31,7 @@ def search(item, texto):
 
 
     item.contentType = 'tvshow'
-    item.url = host + "/search/" + texto
+    item.url = host + "/?s=" + texto
     try:
         return peliculas(item)
     # Continua la ricerca in caso di errore .
