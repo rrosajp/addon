3286638d37f780768e16e513e1afae967573f498
Alhaziel01 <alhaziel01@gmail.com>
2021-06-11 09:59:36 +0200

    Rimosso extended info, non compatibile con Kodi 19

--- platformcode/platformtools.py
+++ platformcode/platformtools.py
@@ -343,16 +343,6 @@ def render_items(itemlist, parent_item):
         if item.action == 'play' and thumb_type == 1 and not item.forcethumb:
             item.thumbnail = config.get_online_server_thumb(item.server)
 
-        # if cloudflare and cloudscraper is used, cookies are needed to display images taken from site
-        # before checking domain (time consuming), checking if tmdb failed (so, images scraped from website are used)
-        # if item.action in ['findvideos'] and not item.infoLabels['tmdb_id']:
-            # faster but ugly way of checking
-            # for d in httptools.FORCE_CLOUDSCRAPER_LIST:
-            #     if d + '/' in item.url:
-            #         item.thumbnail = httptools.get_url_headers(item.thumbnail)
-            #         item.fanart = httptools.get_url_headers(item.fanart)
-            #         break
-
         icon_image = "DefaultFolder.png" if item.folder else "DefaultVideo.png"
         listitem = xbmcgui.ListItem(item.title)
         listitem.setArt({'icon': icon_image, 'thumb': item.thumbnail, 'poster': item.thumbnail,
