2d6a4abc1c67cdc445a3e0df19e597e133f585e0
Alhaziel01 <alhaziel01@gmail.com>
2021-08-23 18:23:31 +0200

    Fix Riproduzione da Videoteca

--- platformcode/platformtools.py
+++ platformcode/platformtools.py
@@ -1778,8 +1778,8 @@ def set_played_time(item):
 def prevent_busy(item):
     logger.debug()
     if not item.autoplay and not item.window:
-        if item.globalsearch: xbmc.Player().play(os.path.join(config.get_runtime_path(), "resources", "kod.mp4"))
-        else: xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=os.path.join(config.get_runtime_path(), "resources", "kod.mp4")))
+        if item.action == 'play_from_library': xbmcplugin.setResolvedUrl(int(sys.argv[1]), True, xbmcgui.ListItem(path=os.path.join(config.get_runtime_path(), "resources", "kod.mp4")))
+        else: xbmc.Player().play(os.path.join(config.get_runtime_path(), "resources", "kod.mp4"))
         # first wait the fake video to start
         while not is_playing():
             xbmc.sleep(10)
@@ -1787,7 +1787,3 @@ def prevent_busy(item):
         xbmc.sleep(200)
         # and then stop it
         xbmc.Player().stop()
-        # xbmc.executebuiltin('Action(Stop)')
-        # xbmc.sleep(500)
-        # xbmc.Player().stop()
-        # xbmc.sleep(500)
