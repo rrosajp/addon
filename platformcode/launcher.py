35f7a50345bd7ddf9e891e4259ea5d6c82cca0e6
Alhaziel01 <alhaziel01@gmail.com>
2021-11-05 17:24:23 +0100

    ooops

--- platformcode/launcher.py
+++ platformcode/launcher.py
@@ -470,10 +470,10 @@ def play_from_library(item):
 
     # logger.debug("item: \n" + item.tostring('\n'))
     # xbmc.Player().play(os.path.join(config.get_runtime_path(), "resources", "kod.mp4"))
-    # if not item.autoplay:
-    #     platformtools.fakeVideo()
-
-    platformtools.prevent_busy(item)
+    if not item.autoplay:
+        platformtools.fakeVideo()
+    # from core.support import dbg;dbg()
+    # platformtools.prevent_busy(item)
 
 
     itemlist=[]
