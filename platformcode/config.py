e0eef19c780b66fff0f768b1bf1dc30cd7865f25
Alhaziel01 <alhaziel01@gmail.com>
2022-01-15 16:14:00 +0100

    Kodi 20 Nexus in get_platform

--- platformcode/config.py
+++ platformcode/config.py
@@ -74,16 +74,16 @@ def get_platform(full_version=False):
     codename = {"10": "dharma", "11": "eden", "12": "frodo",
                 "13": "gotham", "14": "helix", "15": "isengard",
                 "16": "jarvis", "17": "krypton", "18": "leia", 
-                "19": "matrix"}
-    code_db = {'10': 'MyVideos37.db', '11': 'MyVideos60.db', '12': 'MyVideos75.db',
+                "19": "matrix", '20': 'nexus'}
+    video_db = {'10': 'MyVideos37.db', '11': 'MyVideos60.db', '12': 'MyVideos75.db',
                '13': 'MyVideos78.db', '14': 'MyVideos90.db', '15': 'MyVideos93.db',
                '16': 'MyVideos99.db', '17': 'MyVideos107.db', '18': 'MyVideos116.db', 
-               '19': 'MyVideos119.db'}
+               '19': 'MyVideos119.db', '20': 'MyVideos120.db'}
 
     num_version = xbmc.getInfoLabel('System.BuildVersion')
     num_version = re.match("\d+\.\d+", num_version).group(0)
     ret['name_version'] = codename.get(num_version.split('.')[0], num_version)
-    ret['video_db'] = code_db.get(num_version.split('.')[0], "")
+    ret['video_db'] = video_db.get(num_version.split('.')[0], "")
     ret['num_version'] = float(num_version)
     if ret['num_version'] < 14:
         ret['platform'] = "xbmc-" + ret['name_version']
