7eec8ffbc07b03bef8ffb015c6e61091da92c5fc
marco <10120390+mac12m99@users.noreply.github.com>
2022-02-09 20:30:22 +0100

    Fix streamlare

--- servers/streamlare.py
+++ servers/streamlare.py
@@ -2,6 +2,7 @@
 # --------------------------------------------------------
 # Conector streamlare By Alfa development Group
 # --------------------------------------------------------
+import base64
 
 from core import httptools
 from core import scrapertools
@@ -9,21 +10,26 @@ from platformcode import logger, config
 
 
 def test_video_exists(page_url):
+    global data
     logger.info("(page_url='%s')" % page_url)
     response = httptools.downloadpage(page_url)
-    if not response.success or "Not Found" in response.data or "File was deleted" in response.data \
-            or "is no longer available" in response.data:
+    data = response.data
+    if not response.success or "Not Found" in response.data or "File was deleted" in data \
+            or "is no longer available" in data:
         return False, config.get_localized_string(70449) % "Streamlare"
     return True, ""
 
 
 def get_video_url(page_url, premium=False, user="", password="", video_password=""):
+    global data
     logger.info("(page_url='%s')" % page_url)
     video_urls = []
     id = scrapertools.find_single_match(page_url, '/e/(\w+)')
     post = {"id": id}
-    data = httptools.downloadpage("https://streamlare.com/api/video/get", post=post).json
-    media_url = data["result"]["Original"]["src"]
+    token = scrapertools.find_single_match(data, '<meta name="csrf-token" content="([^"]+)')
+    data = httptools.downloadpage("https://streamlare.com/api/video/get", post=post, headers={'X-CSRF-TOKEN': token, 'X-Requested-With': 'XMLHttpRequest'}).json
+    src = data["result"]["Original"]["src"]
+    media_url = ''.join([chr(51 ^ c) for c in base64.b64decode(src)])
     video_urls.append(["MP4", media_url])
     return video_urls
 
