bbd10b5f76417710eebd820f4255c2b79778bee1
Alhaziel01 <alhaziel01@gmail.com>
2022-02-17 17:34:11 +0100

    Fix Riapertura finestra server in caso di errore

--- platformcode/platformtools.py
+++ platformcode/platformtools.py
@@ -2009,9 +2009,10 @@ def serverWindow(item, itemlist):
         if index > 0: xbmc.executebuiltin(context_commands[index])
 
 
+
     if itemlist:
         def monitor(itemlist):
-            reopen = False
+            reopen = True
             while not xbmc.Monitor().abortRequested():
                 if not is_playing():
                     if reopen:
@@ -2028,8 +2029,10 @@ def serverWindow(item, itemlist):
 
                     else:
                         from platformcode.launcher import run
-                        run(selection)
-                        reopen = True
+                        try:
+                            run(selection)
+                        except:
+                            reopen = False
                         if not selection.server or selection.server == 'torrent': break
 
             db.close()
