6aabebb232b60e8d02f77d02af5a63ff440713b1
Alhaziel01 <alhaziel01@gmail.com>
2022-04-20 19:16:08 +0200

    Fix Redirect Dominio

--- platformcode/config.py
+++ platformcode/config.py
@@ -130,6 +130,7 @@ def get_channel_url(findhostMethod=None, name=None, forceFindhost=False):
         return url
     else:
         url = httptools.downloadpage(channels_data['direct'][name], follow_redirects=True, only_headers=True).url
+        if url.endswith('/'): url = url[:-1]
         return url
 
 
