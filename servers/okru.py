0042142878388fb044e62ac3f5a1edfc5d47e313
Alhaziel01 <alhaziel01@gmail.com>
2022-08-02 19:00:59 +0200

    Fix Ok.ru

--- servers/okru.py
+++ servers/okru.py
@@ -9,7 +9,7 @@ from platformcode import logger, config
 
 def test_video_exists(page_url):
     logger.debug("(page_url='%s')" % page_url)
-
+    global data
     data = httptools.downloadpage(page_url).data
     if "copyrightsRestricted" in data or "COPYRIGHTS_RESTRICTED" in data:
         return False, config.get_localized_string(70449) % "OKru"
@@ -23,11 +23,12 @@ def get_video_url(page_url, premium=False, user="", password="", video_password=
     logger.debug("url=" + page_url)
     video_urls = []
 
-    data = httptools.downloadpage(page_url).data
+    # data = httptools.downloadpage(page_url).data
+    global data
     data = scrapertools.decodeHtmlentities(data).replace('\\', '')
     # URL del vídeo
     for type, url in re.findall(r'\{"name":"([^"]+)","url":"([^"]+)"', data, re.DOTALL):
         url = url.replace("%3B", ";").replace("u0026", "&")
-        video_urls.append([type + " [OKru]", url])
+        video_urls.append([type + " [OKru]", url + '|Referrer=' + page_url])
 
     return video_urls
